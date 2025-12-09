42c10744c50072eb188940da2184f054a1ba9645
JihyeKim <jihyekim830@gmail.com>
2025-12-09 17:48:22 +0900

    design: 사이드바 탭 버튼 컴포넌트 CSS 수정 (#16)
    - 클래스명 오타 수정
    - disabled 상태에서 버튼 텍스트 드래그 방지

--- src/components/common/SideBarTapButton.tsx
+++ src/components/common/SideBarTapButton.tsx
@@ -11,8 +11,8 @@ function SideBarTapButton({
       className={cn(
         "w-[152px] py-0.5 pl-5 text-left text-base font-bold text-neutral-400 transition-colors ease-in-out focus:outline-none",
         "hover:text-primary-500 hover:bg-primary-100 hover:rounded-sm",
-        "active:order-l-primary-500 active:rounded-none active:border-l-2 active:bg-white active:pl-4",
-        "disabled:pointer-events-none disabled:rounded-sm disabled:bg-neutral-200 disabled:font-normal disabled:text-neutral-400",
+        "active:border-l-primary-500 active:rounded-none active:border-l-2 active:bg-white",
+        "disabled:pointer-events-none disabled:rounded-sm disabled:bg-neutral-200 disabled:font-normal disabled:text-neutral-400 disabled:select-none",
         className
       )}
       {...props}
