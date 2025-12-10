34794438f5115d317e2f0d70b45b50b2b76dbd9a
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-10 14:26:43 +0900

    feat: 루트 컴포넌트에 ToastBox 추가 (#19) - TODO: 추후에 레이아웃 컴포넌트로 옮기기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import ToastBox from "@/components/common/toast/ToastBox";
 import "./App.css";
 
 function App() {
-  return <div>template</div>;
+  return (
+    <div>
+      {/* 추후 레이아웃으로 아래 컴포넌트 옮기기 */}
+      <ToastBox />
+    </div>
+  );
 }
 
 export default App;
