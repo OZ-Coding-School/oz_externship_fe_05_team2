9d7231313b064ab5cba8a64df066d989e5178c46
nelfen <nelfen.dev@gmail.com>
2025-12-10 23:42:13 +0900

    feat: Error/success 기능 구현 (#23)
    - Error Input 에러 메시지 구현
    - check Input 체크 표시 구현

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,16 +1,17 @@
 import { cn } from "@/lib/utils";
-import type { ComponentProps } from "react";
+import { type InputHTMLAttributes, forwardRef } from "react";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Check } from "lucide-react";
 
 const inputVariants = cva(
-  "rounded-md border-2 px-4 py-3 text-sm placeholder:text-neutral-400 focus:outline-none transition-all duration-200 disabled:bg-neutral-100 disabled:text-neutral-400",
+  "w-full rounded-md border-2 px-4 py-3 text-sm placeholder:text-neutral-400 focus:outline-none transition-all duration-200 disabled:bg-neutral-100 disabled:text-neutral-400",
   {
     variants: {
       variant: {
         default: "border-neutral-300 bg-white focus:border-primary-500",
         danger:
           "border-danger focus:border-danger bg-white placeholder:text-danger",
-        success: "border-success focus:border-success bg-white",
+        success: "border-success focus:border-success bg-white pr-10",
       },
     },
     defaultVariants: {
@@ -20,11 +21,41 @@ const inputVariants = cva(
 );
 
 interface InputProps
-  extends ComponentProps<"input">,
-    VariantProps<typeof inputVariants> {}
-
-export default function Input({ className, variant, ...props }: InputProps) {
-  return (
-    <input className={cn(inputVariants({ variant }), className)} {...props} />
-  );
+  extends InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {
+  errorMessage?: string;
+  inputClassName?: string;
 }
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, inputClassName, variant, errorMessage, ...props }, ref) => {
+    const currentVariant = errorMessage ? "danger" : variant;
+
+    return (
+      <div className={cn("relative", className)}>
+        <div className="relative flex items-center">
+          <input
+            ref={ref}
+            className={cn(
+              inputVariants({ variant: currentVariant }),
+              inputClassName
+            )}
+            {...props}
+          />
+
+          {currentVariant === "success" && (
+            <span className="text-success absolute top-1/2 right-3 -translate-y-1/2 text-lg font-bold">
+              <Check />
+            </span>
+          )}
+        </div>
+        {errorMessage && (
+          <p className="text-danger mt-1 text-xs">*{errorMessage}</p>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
+export default Input;
