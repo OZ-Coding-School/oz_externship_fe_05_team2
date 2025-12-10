006d8507c719f520a4eb7bb1228dd28c27cc5ff1
nelfen <nelfen.dev@gmail.com>
2025-12-10 14:26:07 +0900

    feat: Input 컴포넌트 수정 (#23)
    - border 사이즈 변경
    - 테두리 색 변경
    - Danger 및 Check 글자색 변경

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -3,14 +3,14 @@ import type { ComponentProps } from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const inputVariants = cva(
-  "rounded-md border px-4 py-3 text-sm placeholder:text-neutral-400 focus:outline-none transition-all duration-200 disabled:bg-neutral-100 disabled:text-neutral-400",
+  "rounded-md border-2 px-4 py-3 text-sm placeholder:text-neutral-400 focus:outline-none transition-all duration-200 disabled:bg-neutral-100 disabled:text-neutral-400",
   {
     variants: {
       variant: {
-        default: "border-neutral-200 bg-white focus:border-primary-500",
+        default: "border-neutral-300 bg-white focus:border-primary-500",
         danger:
-          "border-danger text-danger focus:border-danger bg-white placeholder:text-danger",
-        success: "border-success text-success focus:border-success bg-white",
+          "border-danger focus:border-danger bg-white placeholder:text-danger",
+        success: "border-success focus:border-success bg-white",
       },
     },
     defaultVariants: {
