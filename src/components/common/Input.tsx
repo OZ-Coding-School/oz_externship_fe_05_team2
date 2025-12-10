63de290d5bf5e806b96fc728d0770b3c4fa03a73
nelfen <nelfen.dev@gmail.com>
2025-12-10 11:46:38 +0900

    feat: Input 컴포넌트 생성 (#23)
    - Default 외 5개 Input 컴포넌트 작업

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+import type { ComponentProps } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const inputVariants = cva(
+  "w-full rounded-md border px-4 py-3 text-sm placeholder:text-neutral-400 focus:outline-none transition-all duration-200 disabled:bg-neutral-100 disabled:text-neutral-400",
+  {
+    variants: {
+      variant: {
+        default: "border-neutral-200 bg-white focus:border-primary-500",
+        danger:
+          "border-danger text-danger focus:border-danger  bg-white placeholder:text-danger",
+        success: "border-success text-success focus:border-success bg-white",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface InputProps
+  extends ComponentProps<"input">,
+    VariantProps<typeof inputVariants> {}
+
+export default function Input({ className, variant, ...props }: InputProps) {
+  return (
+    <input className={cn(inputVariants({ variant }), className)} {...props} />
+  );
+}
