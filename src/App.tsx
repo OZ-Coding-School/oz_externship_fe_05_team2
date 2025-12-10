c8562e97c2100ccb0c9ca3036ef72e466b7b7152
farmer <lea2047@gmail.com>
2025-12-10 18:42:02 +0900

    feat: NotFound 페이지 컴포넌트 UI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RootLayout } from "@/components/layout";
 import { Home } from "@/pages";
 import { BrowserRouter, Route, Routes } from "react-router";
+import NotFound from "@/pages/NotFound";
 
 function App() {
   return (
@@ -8,10 +9,13 @@ function App() {
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="/" element={<Home />} />
+
           <Route path="/community" element={<div>커뮤니티 페이지</div>} />
           <Route path="/qna" element={<div>질의응답 페이지</div>} />
           <Route path="/profile" element={<div>수강생 등록 페이지</div>} />
           <Route path="/my-page" element={<div>마이페이지</div>} />
+
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
