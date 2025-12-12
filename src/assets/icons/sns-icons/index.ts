1319e7ac507689effaf1f5f929dcb187f43ab2ca
JihyeKim <jihyekim830@gmail.com>
2025-12-13 02:56:51 +0900

    chore: sns 아이콘 파일 추가 (#54)

---
+++ src/assets/icons/sns-icons/index.ts
@@ -0,0 +1,6 @@
+import InstagramIcon from "@/assets/icons/sns-icons/instagram.svg";
+import YoutubeIcon from "@/assets/icons/sns-icons/youtube.svg";
+import NaverBlogIcon from "@/assets/icons/sns-icons/naver-blog.svg";
+import SnsDefaultIcon from "@/assets/icons/sns-icons/sns-default.svg";
+
+export { InstagramIcon, YoutubeIcon, NaverBlogIcon, SnsDefaultIcon };
