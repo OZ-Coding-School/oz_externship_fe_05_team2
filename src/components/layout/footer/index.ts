e8ef93291ba5dc37ca349302bd8ad1084058fd1b
JihyeKim <jihyekim830@gmail.com>
2025-12-13 03:27:55 +0900

    chore: footer 링크 리스트 상수화 (#54)

--- src/components/layout/footer/index.ts
+++ src/components/layout/footer/index.ts
@@ -1,6 +1,13 @@
+import {
+  CAMP_LIST,
+  INFO_LIST,
+  SNS_LIST,
+} from "@/components/layout/footer/constants";
+
 import type {
   FooterLinkItem,
   FooterSnsLinkItem,
 } from "@/components/layout/footer/types";
 
+export { CAMP_LIST, INFO_LIST, SNS_LIST };
 export type { FooterLinkItem, FooterSnsLinkItem };
