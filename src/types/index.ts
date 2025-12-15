a8c6522fb917204add552e68af7200663ff96052
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-15 13:26:26 +0900

    type: 모달 컨텍스트 타입 생성 (#56)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 import type { FooterLinkItem, FooterSnsLinkItem } from "@/types/footer-types";
+import type { ModalContextType } from "@/types/modal-context-type";
 
-export type { FooterLinkItem, FooterSnsLinkItem };
+export type { FooterLinkItem, FooterSnsLinkItem, ModalContextType };
