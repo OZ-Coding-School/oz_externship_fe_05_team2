839d03cb41dc893d8355a5eccceadc6a3c9c1bb6
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-15 13:53:47 +0900

    feat: ModalOverlay 컴포넌트 (#56)

--- src/components/common/modal/index.ts
+++ src/components/common/modal/index.ts
@@ -1,5 +1,6 @@
 import ModalContent from "@/components/common/modal/ModalContent";
 import ModalTrigger from "@/components/common/modal/ModalTrigger";
 import ModalClose from "@/components/common/modal/ModalClose";
+import ModalOverlay from "@/components/common/modal/ModalOverlay";
 
-export { ModalContent, ModalTrigger, ModalClose };
+export { ModalContent, ModalTrigger, ModalClose, ModalOverlay };
