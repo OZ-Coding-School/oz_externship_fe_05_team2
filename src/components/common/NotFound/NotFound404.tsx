27bd4745fe6c0fbf59ab144eadd8dd0be94e381a
farmer <lea2047@gmail.com>
2025-12-15 10:35:26 +0900

    refactor: NotFound404 props사용 (#35)

--- src/components/common/NotFound/NotFound404.tsx
+++ src/components/common/NotFound/NotFound404.tsx
@@ -1,19 +1,24 @@
-export default function NotFound() {
+type NotFoundProps = {
+  className?: string;
+};
+export default function NotFound({ className = "" }: NotFoundProps) {
   return (
-    <div className="w-full flex flex-col items-center py-70  text-center">
+    <div
+      className={`w-full flex flex-col items-center py-70 text-center ${className}`}
+    >
       <div className="text-xl font-bold bg-purple-100 text-purple-700 px-6 py-2 rounded-md mb-6">
         404
       </div>
       <div className="flex flex-col items-center">
-        <div className="w-72 h-72 opacity-40 ">
-            <svg
-              className="w-full h-full stroke-gray-300"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path d="M5 16a4 4 0 0 1 0-8 5 5 0 0 1 9.9-1.2A4.5 4.5 0 1 1 17 16H5z" />
-              <line x1="21" y1="3" x2="3" y2="21" />
-            </svg>
+        <div className="w-72 h-72 opacity-40">
+          <svg
+            className="w-full h-full stroke-gray-300"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path d="M5 16a4 4 0 0 1 0-8 5 5 0 0 1 9.9-1.2A4.5 4.5 0 1 1 17 16H5z" />
+            <line x1="21" y1="3" x2="3" y2="21" />
+          </svg>
         </div>
         <p className="text-gray-500 text-lg font-medium leading-relaxed">
           페이지를 불러올 수 없어요
@@ -21,6 +26,7 @@ export default function NotFound() {
           잠시 뒤 다시 시도해보세요!
         </p>
       </div>
-     </div>
+    </div>
   );
 }
+
