ac20aa72ad06c013a26e89453365ce8a0fb945d0
JihyeKim <jihyekim830@gmail.com>
2025-12-16 15:08:23 +0900

    chore: 과목 아이콘 파일 추가 (#66)

---
+++ src/assets/icons/subject-icons/index.ts
@@ -0,0 +1,31 @@
+import HtmlIcon from "@/assets/icons/subject-icons/html.svg";
+import CssIcon from "@/assets/icons/subject-icons/css.svg";
+import JavascriptIcon from "@/assets/icons/subject-icons/javascript.svg";
+import GithubIcon from "@/assets/icons/subject-icons/github.svg";
+import ReactIcon from "@/assets/icons/subject-icons/react.svg";
+import NodejsIcon from "@/assets/icons/subject-icons/nodejs.svg";
+import DatabaseIcon from "@/assets/icons/subject-icons/database.svg";
+import TypescriptIcon from "@/assets/icons/subject-icons/typescript.svg";
+import AwsIcon from "@/assets/icons/subject-icons/aws.svg";
+import ReactNativeIcon from "@/assets/icons/subject-icons/react-native.svg";
+import PythonIcon from "@/assets/icons/subject-icons/python.svg";
+import DjangoIcon from "@/assets/icons/subject-icons/django.svg";
+import FastapiIcon from "@/assets/icons/subject-icons/fastapi.svg";
+import FlaskIcon from "@/assets/icons/subject-icons/flask.svg";
+
+export {
+  HtmlIcon,
+  CssIcon,
+  JavascriptIcon,
+  GithubIcon,
+  ReactIcon,
+  NodejsIcon,
+  DatabaseIcon,
+  TypescriptIcon,
+  AwsIcon,
+  ReactNativeIcon,
+  PythonIcon,
+  DjangoIcon,
+  FastapiIcon,
+  FlaskIcon,
+};
