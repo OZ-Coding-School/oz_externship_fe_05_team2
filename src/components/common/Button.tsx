cfcf28d31a677a59f2040f86f3f3fc518843b53b
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-17 13:21:07 +0900

    refactor: 버튼 variant를 별도로 분리 (#70)

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,31 +1,6 @@
-import { cn } from "@/lib";
+import { buttonVariants, cn } from "@/lib";
 import type { ComponentProps } from "react";
-import { cva, type VariantProps } from "class-variance-authority";
-
-const buttonVariants = cva(
-  "text-base rounded px-7 py-4 focus:outline-none transition-colors ease-in-out",
-  {
-    variants: {
-      variant: {
-        fill: cn(
-          "text-white bg-primary-500",
-          "hover:bg-primary-600",
-          "active:bg-primary-700",
-          "disabled:text-neutral-400 disabled:bg-neutral-200"
-        ),
-        outline: cn(
-          "text-primary-700 bg-primary-200 border border-primary-500",
-          "hover:bg-primary-300 hover:border-primary-600",
-          "active:bg-primary-400 active:border-primary-700 active:text-primary-800",
-          "disabled:text-neutral-700 disabled:bg-neutral-200 disabled:border-neutral-400"
-        ),
-      },
-    },
-    defaultVariants: {
-      variant: "fill",
-    },
-  }
-);
+import { type VariantProps } from "class-variance-authority";
 
 interface ButtonProps
   extends ComponentProps<"button">,
---
+++ src/lib/buttonVariant.ts
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
+
+export const buttonVariants = cva(
+  "text-base rounded px-7 py-4 focus:outline-none transition-colors ease-in-out",
+  {
+    variants: {
+      variant: {
+        fill: cn(
+          "text-white bg-primary-500",
+          "hover:bg-primary-600",
+          "active:bg-primary-700",
+          "disabled:text-neutral-400 disabled:bg-neutral-200"
+        ),
+        outline: cn(
+          "text-primary-700 bg-primary-200 border border-primary-500",
+          "hover:bg-primary-300 hover:border-primary-600",
+          "active:bg-primary-400 active:border-primary-700 active:text-primary-800",
+          "disabled:text-neutral-700 disabled:bg-neutral-200 disabled:border-neutral-400"
+        ),
+      },
+    },
+    defaultVariants: {
+      variant: "fill",
+    },
+  }
+);
