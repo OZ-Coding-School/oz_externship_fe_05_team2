df5080e15004ff5f8da9d4400965363457c3f674
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-17 14:31:02 +0900

    refactor: NotFound404 컴포넌트에 statusCode prop 추가 (#72)

--- src/components/common/not-found/NotFound404.tsx
+++ src/components/common/not-found/NotFound404.tsx
@@ -2,9 +2,13 @@ import { cn } from "@/lib/utils";
 
 type NotFound404Props = {
   className?: string;
+  statusCode?: number | string;
 };
 
-export default function NotFound404({ className }: NotFound404Props) {
+export default function NotFound404({
+  className,
+  statusCode = 404,
+}: NotFound404Props) {
   return (
     <div
       className={cn(
@@ -13,7 +17,7 @@ export default function NotFound404({ className }: NotFound404Props) {
       )}
     >
       <div className="mb-6 rounded-md bg-purple-100 px-6 py-2 text-xl font-bold text-purple-700">
-        404
+        {statusCode}
       </div>
       <div className="flex flex-col items-center">
         <div className="h-72 w-72 opacity-40">
