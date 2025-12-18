9b2b0eccd729fc7217aed9b00f76f683dc0034b8
JihyeKim <jihyekim830@gmail.com>
2025-12-18 15:37:12 +0900

    feat: Exams 페이지 생성 (#66)

---
+++ src/pages/Exams.tsx
@@ -0,0 +1,59 @@
+import { ExamCategorySelector, ExamList } from "@/components";
+import type { ExamCategory } from "@/types";
+import { useState } from "react";
+import { NotFound } from "@/components/common/not-found";
+import { LoadingUi } from "@/components/common";
+import { useInfiniteScroll } from "@/hooks";
+import { useExamList } from "@/hooks/api";
+
+function Exams() {
+  const [category, setCategory] = useState<ExamCategory>("all");
+
+  const {
+    data: exams,
+    isLoading,
+    isError,
+    error,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useExamList();
+
+  const bottomRef = useInfiniteScroll<HTMLDivElement>(
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+    category === "all"
+  );
+
+  const filteredExams =
+    exams?.pages.flatMap((page) => {
+      if (category === "all") return page.results;
+      return page.results.filter((item) => item.exam_info.status === category);
+    }) ?? [];
+
+  const handleCategoryClick = (category: ExamCategory) => setCategory(category);
+
+  if (isError)
+    return <NotFound statusCode={error.response?.status ?? error.message} />;
+  return (
+    <section className="flex min-h-[60vh] flex-col">
+      <h1 className="mb-10 text-[32px] font-bold">쪽지시험</h1>
+      <ExamCategorySelector
+        category={category}
+        onChange={handleCategoryClick}
+      />
+      {isLoading ? (
+        <LoadingUi className="flex grow items-center self-center" />
+      ) : (
+        <>
+          <ExamList exams={filteredExams} />
+          <div className="h-5" ref={bottomRef}></div>
+          {isFetchingNextPage && <LoadingUi className="mb-10 self-center" />}
+        </>
+      )}
+    </section>
+  );
+}
+
+export default Exams;
