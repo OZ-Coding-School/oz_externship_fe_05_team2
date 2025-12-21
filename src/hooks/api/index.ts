cb27b6fe0e85eb6f923021f4e00a3f4d33ac0561
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-21 10:18:32 +0900

    feat: useEnrollStudent 뮤테이션 훅 (#74)

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -1,5 +1,11 @@
 import useExamList from "@/hooks/api/useExamList";
 import useExamCodeVerification from "@/hooks/api/useExamCodeVerification";
 import useAvailableCourses from "@/hooks/api/useAvailableCourses";
+import useEnrollStudent from "@/hooks/api/useEnrollStudent";
 
-export { useExamList, useExamCodeVerification, useAvailableCourses };
+export {
+  useExamList,
+  useExamCodeVerification,
+  useAvailableCourses,
+  useEnrollStudent,
+};
