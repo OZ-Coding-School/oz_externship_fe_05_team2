7435105652a87eddc0f6842f4eedfe9f0e85face
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-22 14:53:09 +0900

    feat: postVerifyEmail msw (#91)

--- src/mocks/handlers/verification-handlers.ts
+++ src/mocks/handlers/verification-handlers.ts
@@ -24,4 +24,41 @@ const postSendEmail = http.post(
   }
 );
 
-export const verificationHandlers = [postSendEmail];
+const postVerifyEmail = http.post(
+  `${MSW_BASE_URL}${API_PATHS.accounts.verification.verfiyEmail}`,
+  async ({ request }) => {
+    const { email, code } = (await request.clone().json()) as {
+      email: string;
+      code: string;
+    };
+
+    if (!email || !code) {
+      return HttpResponse.json(
+        {
+          error_detail: {
+            email: email ? undefined : ["이 필드는 필수 항목입니다."],
+            code: code ? undefined : ["이 필드는 필수 항목입니다."],
+          },
+        },
+        { status: 400 }
+      );
+    }
+
+    if (code !== "qwer1234") {
+      return HttpResponse.json(
+        {
+          error_detail: {
+            message: "인증코드가 올바르지 않습니다.",
+          },
+        },
+        { status: 403 }
+      );
+    }
+    return HttpResponse.json(
+      { detail: "이메일 인증에 성공했습니다." },
+      { status: 200 }
+    );
+  }
+);
+
+export const verificationHandlers = [postSendEmail, postVerifyEmail];
