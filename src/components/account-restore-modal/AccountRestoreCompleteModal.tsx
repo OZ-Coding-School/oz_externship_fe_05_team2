f523c6e5a1698845e682d34ef97153a7c4fdc8e2
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-22 17:01:14 +0900

    feat: AccountRestoreCompleteModal UI (#93)

--- src/components/account-restore-modal/AccountRestoreCompleteModal.tsx
+++ src/components/account-restore-modal/AccountRestoreCompleteModal.tsx
@@ -1,5 +1,6 @@
 import { Modal, ModalContent } from "@/components/common/modal";
 import type { ModalContextType } from "@/types";
+import { CheckIcon } from "lucide-react";
 
 interface AccountRestoreCompleteModalProps {
   externalModalControl: ModalContextType;
@@ -10,8 +11,13 @@ export default function AccountRestoreCompleteModal({
 }: AccountRestoreCompleteModalProps) {
   return (
     <Modal externalModalControl={externalModalControl}>
-      <ModalContent hasCloseIcon={false}>
-        <span>계정 복구 완료</span>
+      <ModalContent
+        hasCloseIcon={false}
+        className="flex flex-col items-center gap-4"
+      >
+        <CheckIcon className="bg-success size-8 rounded-full p-1 text-white" />
+        <span className="text-xl font-bold">계정 복구 완료!</span>
+        <span className="text-neutral-600">지금 바로 로그인 해보세요.</span>
       </ModalContent>
     </Modal>
   );
