5982789b01cf219a5375482662c8f90ab1b7ed11
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-22 15:19:39 +0900

    feat: useSendSMS hook (#91)

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -4,6 +4,7 @@ import useAvailableCourses from "@/hooks/api/useAvailableCourses";
 import useEnrollStudent from "@/hooks/api/useEnrollStudent";
 import useSendEmail from "@/hooks/api/verification/useSendEmail";
 import useVerifyEmail from "@/hooks/api/verification/useVerifyEmail";
+import useSendSMS from "@/hooks/api/verification/useSendSMS";
 
 export {
   useExamList,
@@ -12,4 +13,5 @@ export {
   useEnrollStudent,
   useSendEmail,
   useVerifyEmail,
+  useSendSMS,
 };
