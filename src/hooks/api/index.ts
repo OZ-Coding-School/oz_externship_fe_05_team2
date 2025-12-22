c9a6f9d7f3bf996de4b7d7f84c13937c277f4003
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-22 15:15:48 +0900

    feat: useSendEmail hook (#91)

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -2,10 +2,12 @@ import useExamList from "@/hooks/api/useExamList";
 import useExamCodeVerification from "@/hooks/api/useExamCodeVerification";
 import useAvailableCourses from "@/hooks/api/useAvailableCourses";
 import useEnrollStudent from "@/hooks/api/useEnrollStudent";
+import useSendEmail from "@/hooks/api/verification/useSendEmail";
 
 export {
   useExamList,
   useExamCodeVerification,
   useAvailableCourses,
   useEnrollStudent,
+  useSendEmail,
 };
