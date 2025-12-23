48415f3c2317bfeee3c9566e8851a7e3a852be8b
farmer <lea2047@gmail.com>
2025-12-23 17:04:32 +0900

    feat: 내 정보 페이지 구현 (#64)

--- src/components/layout/MyPageLayout.tsx
+++ src/components/layout/MyPageLayout.tsx
@@ -1,7 +1,6 @@
   import { useLocation, Outlet } from 'react-router';
   import SideBarTapButton from '@/components/common/SideBarTapButton'; 
 
-
   const menuItems = [
     { id: 'exam', label: '쪽지시험' },
     { id: 'profile', label: '내 정보' },
@@ -14,31 +13,24 @@
       <div className="flex h-screen">
         <aside className="w-1/4 bg-white py-6 flex flex-col space-y-3">
           {menuItems.map(({ id, label }) => {
-    const isActive = currentPathSegment === id;
     return (
       <div key={id} className="ml-70"> 
-<<<<<<< HEAD
         <SideBarTapButton to ={id} isActive={isActive}>
-=======
-        <SideBarTapButton to={id} isActive={isActive}>
->>>>>>> 45088ac (refactor: 공통레이아웃 수정 및 프로필페이지(진행중)추가 (#64))
           {label}
         </SideBarTapButton>
       </div>
     );
-  })}
+     <div key={id} className="ml-70"> 
+       <SideBarTapButton to={id} isActive={currentPathSegment === id}>
+         {label}
+       </SideBarTapButton>
+     </div>
+   );
+  }
         </aside>
         <main className="w-3/4 p-10 overflow-auto bg-white">
           <Outlet /> 
         </main>
       </div>
     );
-<<<<<<< HEAD
-  }
-<<<<<<< HEAD
-
-=======
->>>>>>> 45088ac (refactor: 공통레이아웃 수정 및 프로필페이지(진행중)추가 (#64))
-=======
-  }
->>>>>>> e2d7341 (test: 코드 수정 (#64))
+  
