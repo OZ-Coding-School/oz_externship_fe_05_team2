b9e5890aa901845e48a8bbdbb99db2c66e79c719
nelfen <nelfen.dev@gmail.com>
2025-12-23 11:31:39 +0900

    feat:Schema 수정 후 페이지 반영 (#90)
    - authSchemas.ts에 전화번호 입력 필드 추가
    - 주석 추가

--- src/pages/EmailSignupPage.tsx
+++ src/pages/EmailSignupPage.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { HeaderLogo } from "@/assets/images/logo-images";
-import { Button, Input, LinkButton, Password } from "@/components/common";
+import { Button, Input, Password } from "@/components/common";
 import { SignupSchema, type SignupSchemaType } from "@/schemas/authSchemas";
 import { cn } from "@/lib";
 
@@ -15,30 +15,25 @@ export default function EmailSignupPage() {
     handleSubmit,
     watch,
     setValue,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<SignupSchemaType>({
     resolver: zodResolver(SignupSchema),
     mode: "onChange",
     defaultValues: {
       gender: "M",
+      phone1: "010",
     },
   });
 
-  const nicknameValue = watch("nickname");
-  const emailValue = watch("email");
-  const genderValue = watch("gender");
-
-  const [phone, setPhone] = useState({ p1: "010", p2: "", p3: "" });
-  const isPhoneFilled =
-    phone.p1 && phone.p2.length >= 3 && phone.p3.length >= 4;
+  const values = watch();
 
   const onSubmit = (data: SignupSchemaType) => {
-    console.log("회원가입 데이터:", data);
+    console.log("회원가입 데이터 제출:", data);
   };
 
   return (
     <div className="mx-auto flex min-h-screen max-w-md flex-col items-center px-4 py-12">
-      <div className="mb-2 text-center">
+      <div className="mb-10 text-center">
         <p className="text-lg font-bold text-gray-600">
           마법같이 빠르게 성장시켜줄
         </p>
@@ -46,155 +41,152 @@ export default function EmailSignupPage() {
       </div>
 
       <form className="w-full space-y-6" onSubmit={handleSubmit(onSubmit)}>
-        <section className="space-y-4">
-          <h1 className="mt-6 text-base font-bold text-gray-700">회원가입</h1>
+        <h1 className="text-base font-bold text-gray-700">회원가입</h1>
+        <section>
+          <label className="mb-1 block text-sm">
+            이름<span className="text-red-500">*</span>
+          </label>
+          <Input
+            {...register("name")}
+            errorMessage={errors.name?.message}
+            placeholder="이름을 입력해주세요"
+          />
+        </section>
 
-          <div>
-            <label className="mb-1 block text-sm">
-              이름<span className="text-red-500">*</span>
-            </label>
+        {/* 닉네임 유효성 검사 추가 예정 & 컴포넌트 분리를 통해 마이페이지에서도 사용가능하게 수정 */}
+        <section>
+          <label className="mt-8 mb-1 block text-sm">
+            닉네임<span className="text-red-500">*</span>
+          </label>
+          <div className="flex items-start gap-2">
             <Input
-              {...register("name")}
-              errorMessage={errors.name?.message}
-              placeholder="이름을 입력해주세요"
+              className="flex-1"
+              {...register("nickname")}
+              errorMessage={errors.nickname?.message}
+              placeholder="닉네임을 입력해주세요"
             />
-          </div>
-
-          <div>
-            <label className="mb-1 block text-sm">
-              닉네임<span className="text-red-500">*</span>
-            </label>
-            <div className="flex items-start gap-2">
-              <Input
-                className="flex-1"
-                {...register("nickname")}
-                errorMessage={errors.nickname?.message}
-                placeholder="닉네임을 입력해주세요"
-              />
-              <Button
-                type="button"
-                variant="outline"
-                className="h-12 w-28 p-0"
-                disabled={!nicknameValue}
-              >
-                중복확인
-              </Button>
-            </div>
-          </div>
-
-          <div>
-            <label className="mb-1 block text-sm">
-              생년월일<span className="text-red-500">*</span>
-            </label>
-            <Input
-              type="text"
-              placeholder="8자리 숫자로 입력해주세요 (ex. 20001110)"
-            />
-          </div>
-
-          <div>
-            <label className="mb-1 block text-sm">
-              성별<span className="text-red-500">*</span>
-            </label>
-            <div className="flex gap-2">
-              <Button
-                type="button"
-                variant="outline"
-                className={cn(
-                  "h-10 w-18 rounded-4xl p-0 transition-colors",
-                  genderValue !== "M" &&
-                    "border-neutral-400 bg-neutral-200 text-neutral-700 hover:bg-neutral-300"
-                )}
-                onClick={() => setValue("gender", "M")}
-              >
-                남
-              </Button>
-
-              <Button
-                type="button"
-                variant="outline"
-                className={cn(
-                  "ml-2 h-10 w-18 rounded-4xl p-0 transition-colors",
-                  genderValue !== "F" &&
-                    "border-neutral-400 bg-neutral-200 text-neutral-700 hover:bg-neutral-300"
-                )}
-                onClick={() => setValue("gender", "F")}
-              >
-                여
-              </Button>
-            </div>
-            {errors.gender && (
-              <p className="mt-1 text-xs text-red-500">
-                {errors.gender.message}
-              </p>
-            )}
+            <Button
+              type="button"
+              variant="outline"
+              className="h-12 w-28 p-0"
+              disabled={!values.nickname}
+            >
+              중복확인
+            </Button>
           </div>
         </section>
 
-        <section className="space-y-4">
-          <div>
-            <label className="text-sm">
-              이메일<span className="text-red-500">*</span>
-              <span className="ml-2 text-xs font-bold text-violet-600">
-                로그인 시 아이디로 사용합니다.
-              </span>
-            </label>
-            <div className="flex items-start gap-2">
-              <Input
-                className="flex-1"
-                {...register("email")}
-                errorMessage={errors.email?.message}
-                placeholder="ozcoding@naver.com"
-              />
-              <Button
-                type="button"
-                variant="outline"
-                className="h-12 w-28 p-0"
-                disabled={!!errors.email || !emailValue}
-                onClick={() => setIsEmailSent(true)}
-              >
-                인증코드전송
-              </Button>
-            </div>
-            <div className="mt-2 flex items-center gap-2">
-              <Input className="flex-1" placeholder="인증코드를 입력해주세요" />
-              <Button
-                type="button"
-                variant="outline"
-                className="h-12 w-28 p-0"
-                disabled={!isEmailSent}
-              >
-                인증코드확인
-              </Button>
-            </div>
+        <section>
+          <label className="mt-8 mb-1 block text-sm">
+            생년월일<span className="text-red-500">*</span>
+          </label>
+          <Input
+            {...register("birthday")}
+            errorMessage={errors.birthday?.message}
+            placeholder="8자리 숫자로 입력해주세요 (ex. 20001110)"
+          />
+        </section>
+
+        <section>
+          <label className="mt-8 mb-1 block text-sm">
+            성별<span className="text-red-500">*</span>
+          </label>
+          <div className="flex gap-2">
+            <Button
+              type="button"
+              variant="outline"
+              className={cn(
+                "h-10 w-18 rounded-4xl p-0 transition-colors",
+                values.gender !== "M" &&
+                  "border-neutral-400 bg-neutral-200 text-neutral-700"
+              )}
+              onClick={() => setValue("gender", "M")}
+            >
+              남
+            </Button>
+            <Button
+              type="button"
+              variant="outline"
+              className={cn(
+                "ml-2 h-10 w-18 rounded-4xl p-0 transition-colors",
+                values.gender !== "F" &&
+                  "border-neutral-400 bg-neutral-200 text-neutral-700"
+              )}
+              onClick={() => setValue("gender", "F")}
+            >
+              여
+            </Button>
           </div>
         </section>
 
-        <section className="space-y-4">
+        {/* useSendEmail, useVerifyEmail을 통해 인증 기능 추가 & 컴포넌트 분리를 통해 비밀번호 찾기에서도 사용가능하게 수정 */}
+        <section className="space-y-2">
+          <label className="mb-1 text-sm">
+            이메일<span className="text-red-500">*</span>
+          </label>
+          <span className="ml-2 text-xs font-bold text-violet-600">
+            로그인 시 아이디로 사용합니다.
+          </span>
+
+          <div className="flex items-start gap-2">
+            <Input
+              className="flex-1"
+              {...register("email")}
+              errorMessage={errors.email?.message}
+              placeholder="ozcoding@naver.com"
+            />
+            <Button
+              type="button"
+              variant="outline"
+              className="h-12 w-28 p-0"
+              disabled={!!errors.email || !values.email}
+              onClick={() => setIsEmailSent(true)}
+            >
+              인증코드전송
+            </Button>
+          </div>
+
+          <div className="mt-2 flex items-center gap-2">
+            <Input className="flex-1" placeholder="인증코드를 입력해주세요" />
+            <Button
+              type="button"
+              variant="outline"
+              className="h-12 w-28 p-0"
+              disabled={!isEmailSent}
+            >
+              인증코드확인
+            </Button>
+          </div>
+        </section>
+
+        {/* useSendPhone, useVerifyPhone 파일 추가 예정 & 컴포넌트 분리를 통해 아이디 찾기에서도 사용가능하게 수정 */}
+        <section className="space-y-2">
           <label className="text-sm">
             휴대전화<span className="text-red-500">*</span>
           </label>
           <div className="flex items-center gap-2">
             <Input
               className="w-20 text-center"
-              value={phone.p1}
-              onChange={(e) => setPhone({ ...phone, p1: e.target.value })}
+              {...register("phone1")}
+              inputClassName="text-center"
             />
             <span>-</span>
             <Input
               className="flex-1 text-center"
-              onChange={(e) => setPhone({ ...phone, p2: e.target.value })}
+              {...register("phone2")}
+              inputClassName="text-center"
             />
             <span>-</span>
             <Input
               className="flex-1 text-center"
-              onChange={(e) => setPhone({ ...phone, p3: e.target.value })}
+              {...register("phone3")}
+              inputClassName="text-center"
             />
             <Button
               type="button"
               variant="outline"
               className="h-12 w-28 p-0"
-              disabled={!isPhoneFilled}
+              disabled={!values.phone1 || !values.phone2 || !values.phone3}
               onClick={() => setIsPhoneSent(true)}
             >
               인증번호전송
@@ -216,23 +208,28 @@ export default function EmailSignupPage() {
           </div>
         </section>
 
-        <section className="space-y-4">
+        <section>
           <div className="flex flex-col gap-2">
-            <label className="mb-1 block text-sm">
+            <label className="block text-sm">
               비밀번호<span className="text-red-500">*</span>
               <span className="ml-2 text-xs font-bold text-violet-600">
                 8~15자의 영문 대소문자, 숫자, 특수문자 포함
               </span>
             </label>
-            <Password placeholder="비밀번호를 입력해주세요" />
             <Password
+              {...register("password")}
+              errorMessage={errors.password?.message}
+              placeholder="비밀번호를 입력해주세요"
+            />
+            <Password
+              {...register("passwordConfirm")}
+              errorMessage={errors.passwordConfirm?.message}
               placeholder="비밀번호를 다시 입력해주세요"
-              className="mt-2"
             />
           </div>
         </section>
 
-        <Button type="button" className="mt-2 w-full">
+        <Button type="submit" className="mt-2 w-full" disabled={!isValid}>
           가입하기
         </Button>
       </form>
