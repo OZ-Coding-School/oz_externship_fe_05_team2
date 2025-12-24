9cc44f3928647ffc6330fe67ed3d2c01b33fbb9f
JihyeKim <jihyekim830@gmail.com>
2025-12-25 03:50:38 +0900

    chore: 시험 부정행위 상태 아이콘 추가 (#78)

---
+++ src/assets/icons/cheating-icons/index.ts
@@ -0,0 +1,5 @@
+import CardDefault from "@/assets/icons/cheating-icons/card-default.svg";
+import CardYellow from "@/assets/icons/cheating-icons/card-yellow.svg";
+import CardRed from "@/assets/icons/cheating-icons/card-red.svg";
+
+export { CardDefault, CardYellow, CardRed };
