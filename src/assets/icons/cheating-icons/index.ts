71d808ac0d8cdbacd03bffe1b91206089bae3341
JihyeKim <jihyekim830@gmail.com>
2025-12-25 13:14:08 +0900

    chore: 시험 부정행위 모달 아이콘 추가 (#78)

--- src/assets/icons/cheating-icons/index.ts
+++ src/assets/icons/cheating-icons/index.ts
@@ -1,5 +1,7 @@
 import CardDefault from "@/assets/icons/cheating-icons/card-default.svg";
 import CardYellow from "@/assets/icons/cheating-icons/card-yellow.svg";
 import CardRed from "@/assets/icons/cheating-icons/card-red.svg";
+import ModalYellow from "@/assets/icons/cheating-icons/modal-yellow.svg";
+import ModalRed from "@/assets/icons/cheating-icons/modal-red.svg";
 
-export { CardDefault, CardYellow, CardRed };
+export { CardDefault, CardYellow, CardRed, ModalYellow, ModalRed };
