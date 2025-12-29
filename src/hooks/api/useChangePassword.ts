470c3be283365993443a9d00459ab79e6ee632f0
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-29 14:13:23 +0900

    feat: useChangePassword 성공 및 에러 핸들링 (#133)

--- src/hooks/api/useChangePassword.ts
+++ src/hooks/api/useChangePassword.ts
@@ -1,7 +1,9 @@
 import { API_PATHS, MSW_BASE_URL } from "@/constants";
+import useToast from "@/hooks/useToast";
 import { api } from "@/lib";
 import { useMutation, type UseMutationOptions } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
+import { useNavigate } from "react-router";
 
 type ChangePassordOptions = Omit<
   UseMutationOptions<
@@ -9,16 +11,56 @@ type ChangePassordOptions = Omit<
     AxiosError,
     { oldPassword: string; newPassword: string }
   >,
-  "mutationFn"
+  "mutationFn" | "onSuccess" | "onError"
 >;
 
 export default function useChangePassord(options: ChangePassordOptions) {
+  const { triggerToast } = useToast();
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: async ({ newPassword, oldPassword }) => {
       await api.post(`${MSW_BASE_URL}${API_PATHS.accounts.changePassword}`, {
         old_password: oldPassword,
         new_password: newPassword,
       });
+    },
+    onSuccess: () => {
+      triggerToast({
+        variant: "small",
+        status: "success",
+        text: "비밀번호 변경에 성공했습니다.",
+      });
+      navigate("/my-page/profile");
+    },
+    onError: (error) => {
+      if (error.status === 400) {
+        triggerToast({
+          variant: "small",
+          status: "danger",
+          text: "올바르지 않은 제출 형식입니다.",
+        });
+      } else if (error.status === 401) {
+        triggerToast({
+          variant: "small",
+          status: "danger",
+          text: "로그인 후 이용해주세요.",
+        });
+
+        navigate("/login", { replace: true });
+      } else if (error.status === 500) {
+        triggerToast({
+          variant: "small",
+          status: "danger",
+          text: "서버 에러가 발생했습니다. 잠시후 다시 시도해주세요.",
+        });
+      } else {
+        triggerToast({
+          variant: "small",
+          status: "danger",
+          text: "알 수 없는 에러가 발생했습니다. 잠시후 다시 시도해주세요.",
+        });
+      }
     },
     ...options,
   });
