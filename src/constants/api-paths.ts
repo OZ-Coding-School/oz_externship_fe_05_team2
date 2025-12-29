a17df201522008bb7db253c208055acaeba3521b
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-29 13:37:38 +0900

    feat: 비밀번호 변경 api 상수 생성 (#133)

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -37,5 +37,6 @@ export const API_PATHS = {
       verfiySMS: `${API_PREFIX}${VERIFICATION_API_PREFIX}/verify-sms`,
     },
     me: `${API_PREFIX}/accounts/me`,
+    changePassword: `${API_PREFIX}/accounts/change-password`,
   },
 } as const;
