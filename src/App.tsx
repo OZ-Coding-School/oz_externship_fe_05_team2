4f8189cc54c5b1ecd276029640f1305a615902c8
farmer <lea2047@gmail.com>
2025-12-30 15:08:27 +0900

    feat: 에딧파일 추가 (#64)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ExamLayout, RootLayout } from "@/components/layout";
-import { NotFound } from "@/components/common/not-found";
 import {
   LoginPage,
   SignupPage,
@@ -12,28 +11,10 @@ import ProfileEdit from '@/pages/ProfileEditPage';
 import { BrowserRouter, Route, Routes } from "react-router"; 
 import MyPageLayout from "@/components/layout/MyPageLayout";
 import ProfilePage from '@/pages/ProfilePage'; 
-
+import { NotFound404 } from "@/components/common/not-found"; 
 
 function ExamPage() { return <div>쪽지시험 화면</div>; }
 function PasswordChangePage() { return <div>비밀번호 변경 화면</div>; }
-
-import  { useState } from "react";
-
-
-
-function MyPageWrapper() {
-  const [selectedMenu, setSelectedMenu] = useState('profile');
-
-  const renderContent = () => {
-    switch(selectedMenu) {
-      case 'exam': return <ExamPage />;
-      case 'profile': return <ProfilePage />;
-      case 'password': return <PasswordChangePage />;
-      default: return <ProfilePage />;
-    }
-  };
-
-import { NotFound404 } from "@/components/common/not-found"; 
 
 function App() {
   return (
@@ -44,36 +25,23 @@ function App() {
           <Route path="/community" element={<div>커뮤니티 페이지</div>} />
           <Route path="/qna" element={<div>질의응답 페이지</div>} />
           <Route path="/profile" element={<div>수강생 등록 페이지</div>} />
-          <Route path="/my-page/exams" element={<Exams />} />
-          <Route path="/my-page" element={<div>마이페이지</div>} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/email-signup" element={<EmailSignupPage />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/my-page" />
-          <Route path="/my-page/exams" element={<Exams />} />
-          <Route path="/my-page" />
-          <Route path="/my-page/exams" element={<Exams />} />             
           <Route path="/my-page" element={<MyPageLayout />}>
-          <Route path="exam" element={<ExamPage />} />
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="profileedit" element={<ProfileEdit />} /> 
-          <Route path="password" element={<PasswordChangePage />} />
-      </Route>
-     
-      </Route>
-          <Route path="*" element={<NotFound404 />} />
+            <Route index element={<ProfilePage />} /> 
+            <Route path="exam" element={<ExamPage />} />
+            <Route path="profile" element={<ProfilePage />} />
+            <Route path="profileedit" element={<ProfileEdit />} /> 
+            <Route path="password" element={<PasswordChangePage />} />
+            <Route path="exams" element={<Exams />} />
+          </Route>
+        </Route>
         <Route path="/exam" element={<ExamLayout />}>
-        </Route>
           <Route path=":deploymentId" element={<TakeExam />} />
-        <Route path="/exam" element={<div>쪽지시험 레이아웃</div>}>
-          <Route
-            path=":deploymentId/result/:submissionId"
-            element={<div>쪽지시험 결과</div>}
-          />
+          <Route path=":deploymentId/result/:submissionId" element={<div>쪽지시험 결과</div>} />
         </Route>
-
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound404 />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,14 +1,7 @@
 import RootLayout from "@/components/layout/RootLayout";
-<<<<<<< HEAD
 import ExamLayout from "@/components/layout/ExamLayout";
 
 export { RootLayout, ExamLayout };
 import MyPageLayout from "@/components/layout/MyPageLayout";
 export { MyPageLayout };
 
-=======
-import MyPageLayout from "@/components/layout/MyPageLayout";
-
-export { RootLayout };
-export { MyPageLayout };
->>>>>>> 45088ac (refactor: 공통레이아웃 수정 및 프로필페이지(진행중)추가 (#64))
