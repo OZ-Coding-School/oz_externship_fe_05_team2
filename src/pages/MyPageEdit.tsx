5059050b5766aeb14979cf6511df81fd756f40e5
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-02 00:47:55 +0900

    feat: 이미지 업로드 에러 핸들링 및 프리뷰 (#147)

--- src/pages/MyPageEdit.tsx
+++ src/pages/MyPageEdit.tsx
@@ -1,14 +1,27 @@
 import { UserIcon } from "@/assets/icons/interface-icons";
 import { Button, Input, LoadingUi } from "@/components/common";
+import { useToast } from "@/hooks";
 import { useUserInformation } from "@/hooks/api";
 import { cn, creatProfileImageUrl } from "@/lib/utils";
 import { CameraIcon } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function MyPageEdit() {
   const { data: user, isPending } = useUserInformation();
 
   const [imageError, setImageError] = useState(false);
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+  const [image, setImage] = useState<File | null>(null);
+
+  const { triggerToast } = useToast();
+
+  useEffect(() => {
+    return () => {
+      if (previewImage) {
+        URL.revokeObjectURL(previewImage);
+      }
+    };
+  }, [previewImage]);
 
   if (isPending) {
     return (
@@ -38,6 +51,38 @@ export default function MyPageEdit() {
 
   const profileImageUrl = creatProfileImageUrl(userId);
 
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+    const ALLOWED_FILE_TYPES = ["image/jpeg", "image/png"];
+
+    if (!ALLOWED_FILE_TYPES.includes(file.type)) {
+      triggerToast({
+        variant: "small",
+        text: "이미지 파일은 png, jpg 형식만 가능합니다.",
+        status: "danger",
+      });
+      e.target.value = "";
+      return;
+    }
+
+    if (file.size > MAX_FILE_SIZE) {
+      triggerToast({
+        variant: "small",
+        text: "이미지 파일 크기는 5MB를 초과할 수 없습니다.",
+        status: "danger",
+      });
+      e.target.value = "";
+      return;
+    }
+
+    const newPreviewUrl = URL.createObjectURL(file);
+    setPreviewImage(newPreviewUrl);
+    setImage(file);
+  };
+
   return (
     <form className="flex flex-col gap-5">
       <div className="flex items-center justify-between">
@@ -57,19 +102,29 @@ export default function MyPageEdit() {
 
           <div className="relative flex items-center justify-center">
             <img
-              src={imageError || !profileImageUrl ? UserIcon : profileImageUrl}
+              src={
+                previewImage ||
+                (imageError || !profileImageUrl ? UserIcon : profileImageUrl)
+              }
               alt="프로필 이미지"
               width={184}
               height={184}
-              className="w-full max-w-44"
+              className="aspect-square h-full max-h-44 w-full max-w-44 rounded-full object-cover object-center"
               onError={() => {
-                setImageError(true);
+                if (!previewImage) {
+                  setImageError(true);
+                }
               }}
             />
 
             <label className="absolute right-2 bottom-2 flex cursor-pointer items-center justify-center rounded-full border-3 border-white bg-neutral-400 p-1.5 transition-colors hover:bg-neutral-500">
               <CameraIcon className="text-white" />
-              <input type="file" className="hidden" />
+              <input
+                type="file"
+                className="hidden"
+                accept="image/png, image/jpeg"
+                onChange={handleImageChange}
+              />
             </label>
           </div>
 
