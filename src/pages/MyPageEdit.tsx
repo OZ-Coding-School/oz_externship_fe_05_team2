c394e29100058539f0419c630735dcbb734f1f49
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-02 00:01:34 +0900

    feat: 내 정보 수정 UI (#147)

--- src/pages/MyPageEdit.tsx
+++ src/pages/MyPageEdit.tsx
@@ -1,3 +1,164 @@
+import { UserIcon } from "@/assets/icons/interface-icons";
+import { Button, Input, LoadingUi } from "@/components/common";
+import { useUserInformation } from "@/hooks/api";
+import { cn, creatProfileImageUrl } from "@/lib/utils";
+import { CameraIcon } from "lucide-react";
+import { useState } from "react";
+
 export default function MyPageEdit() {
-  return <div>MyPageEdit</div>;
+  const { data: user, isPending } = useUserInformation();
+  const [imageError, setImageError] = useState(false);
+
+  if (isPending) {
+    return (
+      <div className="flex items-center justify-center">
+        <LoadingUi />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center">
+        유저 데이터를 불러오는데 문제가 발생했습니다. 잠시후 다시 시도해주세요.
+      </div>
+    );
+  }
+
+  const {
+    nickname,
+    email,
+    name,
+    phone_number: phoneNumber,
+    gender,
+    birthday,
+    id: userId,
+  } = user;
+
+  const profileImageUrl = creatProfileImageUrl(userId);
+
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">내 정보</h1>
+        <Button>저장하기</Button>
+      </div>
+
+      <section className="flex flex-col items-center gap-20 rounded-lg border p-11">
+        {/* 프로필 */}
+        <div className="flex w-full flex-col items-center gap-10">
+          <div className="flex w-full flex-col items-center gap-2">
+            <span className="text-primary-600 w-full text-xl font-semibold">
+              프로필
+            </span>
+            <hr className="w-full" />
+          </div>
+
+          <div className="relative flex items-center justify-center">
+            <img
+              src={imageError || !profileImageUrl ? UserIcon : profileImageUrl}
+              alt="프로필 이미지"
+              width={184}
+              height={184}
+              className="w-full max-w-44"
+              onError={() => {
+                setImageError(true);
+              }}
+            />
+
+            <label className="absolute right-2 bottom-2 flex cursor-pointer items-center justify-center rounded-full border-3 border-white bg-neutral-400 p-1.5 transition-colors hover:bg-neutral-500">
+              <CameraIcon className="text-white" />
+              <input type="file" className="hidden" />
+            </label>
+          </div>
+
+          <div className="flex w-full flex-col gap-5">
+            <div className="flex w-full flex-col gap-2">
+              <label>닉네임</label>
+              <div className="flex w-full items-center gap-2">
+                <Input defaultValue={nickname} className="flex-1" />
+                <Button
+                  className="flex h-11 items-center justify-center"
+                  variant={"outline"}
+                >
+                  중복확인
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex w-full flex-col gap-2">
+              <label>이메일</label>
+              <Input defaultValue={email} disabled />
+            </div>
+          </div>
+        </div>
+
+        {/* 개인정보 */}
+        <div className="flex w-full flex-col items-center gap-10">
+          <div className="flex w-full flex-col items-center gap-2">
+            <span className="text-primary-600 w-full text-xl font-semibold">
+              개인정보
+            </span>
+            <hr className="w-full" />
+          </div>
+
+          <div className="flex w-full flex-col gap-5">
+            <div className="flex w-full flex-col gap-2">
+              <label>이름</label>
+              <Input defaultValue={name} />
+            </div>
+
+            <div className="flex w-full flex-col gap-2">
+              <label>휴대전화</label>
+              <div className="flex w-full items-center gap-2">
+                <Input
+                  defaultValue={`${phoneNumber.slice(0, 3)}-${phoneNumber.slice(3, 7)}-${phoneNumber.slice(7)}`}
+                  className="flex-1"
+                />
+                <Button
+                  className="flex h-11 items-center justify-center"
+                  variant={"outline"}
+                >
+                  인증번호 전송
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex w-full flex-col gap-2">
+              <label>성별</label>
+              <div className="flex gap-2">
+                <Button
+                  type="button"
+                  variant="outline"
+                  className={cn(
+                    "h-10 w-18 rounded-4xl p-0 transition-colors",
+                    gender !== "M" &&
+                      "border-neutral-400 bg-neutral-200 text-neutral-700"
+                  )}
+                >
+                  남
+                </Button>
+                <Button
+                  type="button"
+                  variant="outline"
+                  className={cn(
+                    "ml-2 h-10 w-18 rounded-4xl p-0 transition-colors",
+                    gender !== "F" &&
+                      "border-neutral-400 bg-neutral-200 text-neutral-700"
+                  )}
+                >
+                  여
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex w-full flex-col gap-2">
+              <label>생년월일</label>
+              <Input defaultValue={birthday} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
